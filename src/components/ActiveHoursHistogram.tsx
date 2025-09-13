7c878b455af3c62b945ce06605c99fd52e618f45
[dyad] <git@dyad.sh>
2025-09-13 17:23:10 +0500

    [dyad] Fixed syntax error in ActiveHoursHistogram by removing misplaced comment. - wrote 1 file(s)

--- src/components/ActiveHoursHistogram.tsx
+++ src/components/ActiveHoursHistogram.tsx
@@ -78,7 +78,6 @@ const ActiveHoursHistogram = () => {
                 className="text-xs text-muted-foreground"
                 width={40}
                 ticks={[0, 2, 4, 6, 8]}
-                {/* Added 6 to the ticks array */}
               />
               <Tooltip
                 cursor={{ fill: "transparent" }}
