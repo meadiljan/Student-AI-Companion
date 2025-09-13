fe9c4fbfbd2981ae73cd0a2fb0a7ddff755d6bfa
[dyad] <git@dyad.sh>
2025-09-13 17:26:58 +0500

    [dyad] wrote 1 file(s)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,13 +10,12 @@ const Dashboard = () => {
     <div className="flex h-full flex-col p-4">
       <DashboardHeader />
       <OverviewCards />
-      <div className="grid gap-6 lg:grid-cols-3 flex-1 mb-6">
-        <div className="lg:col-span-2 flex flex-col gap-6">
-          {/* Removed PerformanceOverview component */}
+      <div className="grid gap-6 lg:grid-cols-5 flex-1 mb-6"> {/* Changed to lg:grid-cols-5 */}
+        <div className="lg:col-span-3 flex flex-col gap-6"> {/* Changed to lg:col-span-3 */}
           <ActiveHoursHistogram />
           <AssignmentsList />
         </div>
-        <div className="lg:col-span-1 flex flex-col">
+        <div className="lg:col-span-2 flex flex-col"> {/* Changed to lg:col-span-2 */}
           <UpcomingEvents />
         </div>
       </div>
