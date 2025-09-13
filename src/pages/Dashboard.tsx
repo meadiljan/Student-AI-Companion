f0d9fd723667dc3eec94cc88115566bb1e5528ee
[dyad] <git@dyad.sh>
2025-09-13 17:25:40 +0500

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,8 @@
 import DashboardHeader from "@/components/DashboardHeader";
 import OverviewCards from "@/components/OverviewCards";
 import UpcomingEvents from "@/components/UpcomingEvents";
-import PerformanceOverview from "@/components/PerformanceOverview";
 import AssignmentsList from "@/components/AssignmentsList";
-import ActiveHoursHistogram from "@/components/ActiveHoursHistogram"; // Import the new component
+import ActiveHoursHistogram from "@/components/ActiveHoursHistogram";
 import { MadeWithDyad } from "@/components/made-with-dyad";
 
 const Dashboard = () => {
@@ -13,8 +12,8 @@ const Dashboard = () => {
       <OverviewCards />
       <div className="grid gap-6 lg:grid-cols-3 flex-1 mb-6">
         <div className="lg:col-span-2 flex flex-col gap-6">
-          <PerformanceOverview />
-          <ActiveHoursHistogram /> {/* Added the new histogram here */}
+          {/* Removed PerformanceOverview component */}
+          <ActiveHoursHistogram />
           <AssignmentsList />
         </div>
         <div className="lg:col-span-1 flex flex-col">
