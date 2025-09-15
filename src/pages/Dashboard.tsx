633bf5ae4a253f7e76575598bc74d0173dd0c0a6
[dyad] <adiljanbusiness@gmail.com>
2025-09-16 01:27:51 +0500

    Reverted all changes back to version 32faef2b1dcd2430a4de68b1b6dd873ca8f51a45

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,10 +10,12 @@ const Dashboard = () => {
     <div className="flex h-full flex-col p-4 pb-2">
       <DashboardHeader />
       <OverviewCards />
-      <div className="flex flex-col gap-6 flex-1 min-h-0">
-        <AssignmentsList />
-        <div className="grid lg:grid-cols-2 gap-6 flex-1">
+      <div className="grid gap-6 lg:grid-cols-6 flex-1 min-h-0">
+        <div className="lg:col-span-3 flex flex-col gap-6 min-h-0">
+          <AssignmentsList />
           <ActiveHoursHistogram />
+        </div>
+        <div className="lg:col-span-3 flex flex-col min-h-0">
           <UpcomingEvents />
         </div>
       </div>
