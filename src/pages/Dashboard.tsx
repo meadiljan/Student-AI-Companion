56d9078a1003c804a5aa8ba7492f5c30356f1253
[dyad] <adiljanbusiness@gmail.com>
2025-09-16 01:34:25 +0500

    [dyad] Balanced dashboard component heights - wrote 4 file(s)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,12 +11,14 @@ const Dashboard = () => {
       <DashboardHeader />
       <OverviewCards />
       <div className="grid gap-6 lg:grid-cols-6 flex-1 min-h-0">
-        <div className="lg:col-span-3 flex flex-col gap-6 min-h-0">
+        <div className="lg:col-span-3 flex flex-col gap-6">
           <AssignmentsList />
           <ActiveHoursHistogram />
         </div>
-        <div className="lg:col-span-3 flex flex-col min-h-0">
-          <UpcomingEvents />
+        <div className="lg:col-span-3 flex flex-col">
+          <div className="flex flex-col h-full">
+            <UpcomingEvents />
+          </div>
         </div>
       </div>
       <MadeWithDyad />
