d8916b36a5adc718411dc23991f90759e5dd2fd8
[dyad] <adiljanbusiness@gmail.com>
2025-09-16 01:27:34 +0500

    [dyad] Balanced dashboard layout - wrote 2 file(s)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,12 +10,10 @@ const Dashboard = () => {
     <div className="flex h-full flex-col p-4 pb-2">
       <DashboardHeader />
       <OverviewCards />
-      <div className="grid gap-6 lg:grid-cols-6 flex-1 min-h-0">
-        <div className="lg:col-span-3 flex flex-col gap-6 min-h-0">
-          <AssignmentsList />
+      <div className="flex flex-col gap-6 flex-1 min-h-0">
+        <AssignmentsList />
+        <div className="grid lg:grid-cols-2 gap-6 flex-1">
           <ActiveHoursHistogram />
-        </div>
-        <div className="lg:col-span-3 flex flex-col min-h-0">
           <UpcomingEvents />
         </div>
       </div>
